da437c88420554d36b312fde2f33e7f748d2d424
vangertorn <vangertorn@yandex.ru>
2021-03-23 10:11:11 +0300

    testCommit2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        This is test commit
+//        This is test commit with new branch:)
 
     }
 }
